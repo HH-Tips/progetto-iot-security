23e2a0dc794965a9402cca7521fa338dd6bb1212
HH-Tips <marco.pati2000@libero.it>
2026-07-09 20:41:11 +0200

    feat: implement firmware flashing sequence and firmware_loader

--- tools/firmware_loader.py
+++ tools/firmware_loader.py
@@ -1,3 +1,4 @@
+from pwnlib.replacements import sleep
 from pwn import *
 from tools.uboot_shell import open_uboot_shell
 
@@ -7,6 +8,13 @@ print("Loading Firmware...")
 sleep(0.3)
 p.sendline(b"tftp 0x81000000 new-firmware.bin")
 p.recvuntil(b"done")
+sleep(0.3)
+p.sendline(b"erase 0x9f020000 +3c0000")
+p.recvuntil(b"Erased 60 sectors")
+sleep(0.3)
+p.sendline(b"cp.b 0x81000000 0x9f020000 3c0000")
+p.recvuntil(b"done")
+sleep(0.3)
 print("Rebooting...")
-p.sendline(b"bootm 0x81000000")
+p.sendline(b"reset")
 p.interactive()
