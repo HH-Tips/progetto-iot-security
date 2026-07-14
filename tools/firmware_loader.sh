dc468935cb592e895fbcae53e7a0b8c9b7a458cc
HH-Tips <marco.pati2000@libero.it>
2026-07-14 21:04:50 +0200

    firmware_loader improvement

--- tools/firmware_loader.sh
+++ tools/firmware_loader.sh
@@ -8,10 +8,25 @@ tools/setup_network.sh
 systemctl enable tftpd.service
 systemctl restart tftpd.service
 
-cp fmk/new-firmware.bin /srv/tftp/new-firmware.bin
+FIRMWARE=$1
+
+if [ $# -lt 1 ]; then
+    FIRMWARE="fmk/new-firmware.bin"
+else
+    echo "Firmware Override"
+fi
+
+dimensione=$(du -sh "$FIRMWARE" | cut -f1)
+if [ "$dimensione" = "3,9M" ]; then
+    echo "Cutting Firmware"
+    dd if=$FIRMWARE of=/dev/shm/new-firmware_cut.bin bs=1 skip=131584
+    cp /dev/shm/new-firmware_cut.bin /srv/tftp/new-firmware.bin
+else
+    cp $FIRMWARE /srv/tftp/new-firmware.bin
+fi
 
 .venv/bin/python -m tools.firmware_loader
 
 tools/reset_network.sh
-systemctl stop tftpd.service
-systemctl disable tftpd.service
+# systemctl stop tftpd.service
+# systemctl disable tftpd.service
